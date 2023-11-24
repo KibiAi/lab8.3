438291d83c3eeb18bcf16210da63f8bbbe3de258
KibiAi <vetal2004v@gmail.com>
2023-11-24 13:05:55 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using lab8._3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть тип продукту (Smartphone, Laptop, Tablet):");
+        string productType = Console.ReadLine();
+
+        TechProductFactory factory;
+        switch (productType)
+        {
+            case "Smartphone":
+                factory = new SmartphoneFactory();
+                break;
+            case "Laptop":
+                factory = new LaptopFactory();
+                break;
+            case "Tablet":
+                factory = new TabletFactory();
+                break;
+            default:
+                Console.WriteLine("Невідомий тип продукту");
+                return;
+        }
+
+        TechProduct product = factory.CreateTechProduct();
+        product.Assemble();
+    }
+}
---
+++ TechProduct.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8._3
+{
+    public abstract class TechProduct
+    {
+        public abstract void Assemble();
+    }
+
+    public class Smartphone : TechProduct
+    {
+        public override void Assemble()
+        {
+            Console.WriteLine("Смартфон зібрано");
+        }
+    }
+
+    public class Laptop : TechProduct
+    {
+        public override void Assemble()
+        {
+            Console.WriteLine("Ноутбук зібрано");
+        }
+    }
+
+    public class Tablet : TechProduct
+    {
+        public override void Assemble()
+        {
+            Console.WriteLine("Планшет зібрано");
+        }
+    }
+
+    public abstract class TechProductFactory
+    {
+        public abstract TechProduct CreateTechProduct();
+    }
+
+    public class SmartphoneFactory : TechProductFactory
+    {
+        public override TechProduct CreateTechProduct()
+        {
+            return new Smartphone();
+        }
+    }
+
+    public class LaptopFactory : TechProductFactory
+    {
+        public override TechProduct CreateTechProduct()
+        {
+            return new Laptop();
+        }
+    }
+
+    public class TabletFactory : TechProductFactory
+    {
+        public override TechProduct CreateTechProduct()
+        {
+            return new Tablet();
+        }
+    }
+
+}
